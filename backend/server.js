7c1b9a43bcbd63a8d639e97b4ce3ce8b48019f4a
ngodingbentar <dewaqintoro@gmail.com>
2023-10-31 15:21:59 +0700

    updated: rooter

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1', apiRouter)
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
 app.get('/', (req, res) => {
   res.send('server is ready')
 })
